903255077ed310bbcc9aae8aa31802747ef745b5
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-10-21 13:11:01 +0200

    Initial commit containing examples and presentation

---
+++ 09-SourceGenerators/09-SourceGenerators.Demo/HttpClient.cs
@@ -0,0 +1,10 @@
+﻿using _09_SourceGenerators.Attributes;
+
+namespace _09_SourceGenerators.Demo
+{
+    [SwaggerClient(SwaggerDescriptor = "http://localhost:59905/swagger/v1/swagger.json")]
+    public partial class HttpClient
+    {
+
+    }
+}
